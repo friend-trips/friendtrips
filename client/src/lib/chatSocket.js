5748ee2f8b9add1f49973043342ef1d6a8cf2b64
phileasthefogg <phileasthefogg@gmail.com>
2021-02-05 20:04:45 -0800

    added env switch for chatSocket

--- client/src/lib/chatSocket.js
+++ client/src/lib/chatSocket.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000', {
+const serverURL = process.env.PORT ? `https://friendtrips.herokuapp.com/${process.env.PORT}` : 'http://localhost:4000';
+const socket = io(serverURL, {
   path: '/socket.io',
   auth: {
     user_id: '',
