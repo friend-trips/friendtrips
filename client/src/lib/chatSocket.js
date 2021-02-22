d97ac5460378ecd71040e49bc29c992bee4e81cb
phileasthefogg <phileasthefogg@gmail.com>
2021-02-21 20:52:54 -0800

    attempt

--- client/src/lib/chatSocket.js
+++ client/src/lib/chatSocket.js
@@ -1,7 +1,15 @@
 import io from 'socket.io-client';
-console.log('chatsocketport', process.env.PORT)
+console.log('chatsocketport', process.env.PORT) // can't find this .env var
 const serverURL = process.env.PORT ? `https://friendtrips.herokuapp.com/${process.env.PORT}` : 'http://localhost:4000';
-const socket = io(serverURL, {
+// const socket = io(serverURL, {
+//   path: '/socket.io',
+//   auth: {
+//     user_id: '',
+//     username: ''
+//   },
+//   autoConnect: false
+// });
+const socket = io('/', {
   path: '/socket.io',
   auth: {
     user_id: '',
